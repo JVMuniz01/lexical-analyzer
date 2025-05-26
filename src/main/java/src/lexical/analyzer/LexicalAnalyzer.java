84cedb9f482f2ed8c2b3dcac6fae7798eef7141b
Muniz <j040vmr@gmail.com>
2025-05-26 19:41:40 -0300

    First commit - Token and Lexical

---
+++ src/main/java/src/lexical/analyzer/LexicalAnalyzer.java
@@ -0,0 +1,10 @@
+package src.lexical.analyzer;
+
+import lexicalAnalysis.Lexicon;
+
+public class LexicalAnalyzer {
+
+    public static void main(String[] args) {
+        Lexicon lexicon = new Lexicon("C:\\ws-sts\\lexical-analyzer\\src\\main\\java\\src\\lexical\\analyzer\\code.txt");
+    }
+}
